abe10860e9a6673e53a966bb91acc1b3832149d8
rampgingmoose <76532218+rampgingmoose@users.noreply.github.com>
2023-05-31 12:33:55 -0400

    Project Creation

---
+++ ActionRoguelite/Source/ActionRoguelite/ActionRogueliteGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueliteGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELITE_API AActionRogueliteGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
